43459e03939fb8584619fafb0b658baeb250d021
Bryan McCormick <bryan@mccormi.com>
2018-12-12 16:47:53 -0800

    dev

--- generic-site/src/main/javascript/app/index.js
+++ generic-site/src/main/javascript/app/index.js
@@ -1,4 +1,6 @@
 $ = require('jquery');
+require('bootstrap');
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 const TimeDisplay = require('./components/util/TimeDisplay');
